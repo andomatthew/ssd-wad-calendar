92af0fc8aca30e69a8c66ab34a98b75e1e933fd1
Alexandro M <alexandro.matthew@gmail.com>
2023-07-08 01:56:57 +0700

    init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from "react"
+
+import "./assets/main.css"
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      Hello Vite React
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  )
+}
+
+export default App
